0ba9713ff4079e5909857abd96328739096d9933
joaovictormss <122957685+joaovictormss@users.noreply.github.com>
2025-10-24 21:31:36 -0300

    Adição da API
    Fim do programa

--- 123.py
+++ 123.py
@@ -1,13 +1,33 @@
 import tkinter
 import PyPDF2
+import requests
+import re
 
 def Leitor(in_PdfFile):  
     reader = PyPDF2.PdfReader(in_PdfFile) 
     print(reader.pages[0].extract_text())
 
 
+    url = "https://date.nager.at/api/v3/PublicHolidays/2025/BR"
+
+    payload = {}
+    headers = {
+  'accept': 'application/json',
+  'X-CSRF-TOKEN': 'pYBqfz7tfH5NFeqA2YXNhdZIsqRCMmef6FjOTNJz'
+  }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+    feriado= reader.pages[0].extract_text().split("\n")
+    for data in feriado:
+        if data.strip() in response.text:
+            print (data)
+
+
 def LerPDF():
     Leitor(r"C:\Users\Aluno.ESTACIOACAD\Downloads\Trabalho\Trabalho\Lista de datas.pdf")
+
 
 def Exemplo():
     root = tkinter.Tk()
